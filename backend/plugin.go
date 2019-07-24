fb09e89c63f793cbaf17fb34ab512e61e2024d2f
Roger Huebner <roger.huebner@microfocus.com>
2019-07-24 15:31:16 -0500

    support two new environment varables:
    VERTICA_GRAFANA_LOG_FILE -
    the output file for the vertica grafana backend binary
    log messages
    
    VERTICA_GRAFANA_LOG_LEVEL -
    the minimum log level the be logged
    the default is "INFO"

--- backend/plugin.go
+++ backend/plugin.go
@@ -1,26 +1,45 @@
 package main
 
 import (
+	"bufio"
+	"io/ioutil"
+	"os"
+	"strings"
+
 	"github.com/grafana/grafana_plugin_model/go/datasource"
 	hclog "github.com/hashicorp/go-hclog"
 	plugin "github.com/hashicorp/go-plugin"
 )
 
+func initializeLogger() hclog.Logger {
+	logOutput := ioutil.Discard
+
+	if filename, set := os.LookupEnv("VERTICA_GRAFANA_LOG_FILE"); set {
+		if f, err := os.Create(filename); err == nil {
+			logOutput = f
+		}
+	}
+
+	logLevel := hclog.LevelFromString("INFO")
+
+	if logEnv, set := os.LookupEnv("VERTICA_GRAFANA_LOG_LEVEL"); set {
+		if newLevel := hclog.LevelFromString(strings.ToUpper(logEnv)); newLevel != hclog.NoLevel {
+			logLevel = newLevel
+		}
+	}
+
+	return hclog.New(&hclog.LoggerOptions{
+		Name:   "vertica-grafana-plugin",
+		Level:  logLevel,
+		Output: bufio.NewWriter(logOutput),
+	})
+}
+
 func main() {
 
-	// f, err := os.Create("/home/ambival/log.txt")
+	logger := initializeLogger()
 
-	// if err != nil {
-	// 	os.Exit(0)
-	// }
-
-	var logger = hclog.New(&hclog.LoggerOptions{
-		Name:  "vertica-grafana-plugin",
-		Level: hclog.LevelFromString("DEBUG"),
-		// Output: bufio.NewWriter(f),
-	})
-
-	logger.Debug("Starting Vertica datasource plugin")
+	logger.Info("Starting Vertica datasource plugin")
 
 	verticaDatasource, err := newVerticaDatasource(logger)
 
