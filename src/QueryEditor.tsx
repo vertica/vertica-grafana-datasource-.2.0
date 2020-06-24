f8137666f88dbe6c3ee780a665e6a0eeba9d189c
Christian Mesh <Christian.Mesh@coxautoinc.com>
2020-06-24 12:05:32 -0400

    Use correct brace/ace to prevent conflicts

--- src/QueryEditor.tsx
+++ src/QueryEditor.tsx
@@ -1,22 +1,17 @@
 import defaults from 'lodash/defaults';
 
-import React, { ChangeEvent, PureComponent } from 'react';
+import React, { PureComponent } from 'react';
 import { QueryEditorProps } from '@grafana/data';
 import { DataSource } from './DataSource';
 import { defaultQuery, VerticaDataSourceOptions, VerticaQuery } from './types';
 import AceEditor from 'react-ace';
 
-import 'ace-builds/src-noconflict/mode-sql';
+import 'brace/mode/sql';
 
 type Props = QueryEditorProps<DataSource, VerticaQuery, VerticaDataSourceOptions>;
 
 export class QueryEditor extends PureComponent<Props> {
-  onQueryTextChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { onChange, query } = this.props;
-    onChange({ ...query, rawSql: event.target.value });
-  };
-
-  _onQueryTextChange = (event: string) => {
+  onQueryTextChange = (event: string) => {
     const { onChange, query } = this.props;
     onChange({ ...query, rawSql: event });
   };
@@ -25,13 +20,13 @@ export class QueryEditor extends PureComponent<Props> {
     const query = defaults(this.props.query, defaultQuery);
     const { rawSql } = query;
     // @ts-ignore
-    ace.config.set('basePath', 'public/app/core/components/code_editor/');
+    //ace.config.set('basePath', 'public/app/core/components/code_editor/');
     return (
       <div className="gf-form">
         <AceEditor
           theme={'grafana-dark'}
           mode={'sql'}
-          onChange={this._onQueryTextChange}
+          onChange={this.onQueryTextChange}
           value={rawSql || ''}
           maxLines={10}
           tabSize={2}
